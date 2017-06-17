737a110d9b4156d80f2df24f40fa04456f5fe9e1
Karina Franco <karinafraest@gmail.com>
2017-06-17 01:34:36 -0500

    add styles using props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -6,7 +6,7 @@ var MyTitle = React.createClass({
 	render: function() {
 		return(
 			div(null,
-				h1(null, this.props.title)
+				h1({style: {color: this.props.color, fontweight: "bold"} }, this.props.title)
 			)
 		)
 	}
@@ -18,10 +18,10 @@ var MyFirstComponent = React.createClass({
 	render: function(){
 		return (
 			div(null,
-				MyTitleFactory({title: "props are the best"}),
-				MyTitleFactory({title: "semicolons are the worst"}),
-				MyTitleFactory({title: "its ok if you use semicolons"}),
-				MyTitleFactory({title: "i am a title as well"})
+				MyTitleFactory({title: "props are the best", color: "peru"}),
+				MyTitleFactory({title: "semicolons are the worst", color: "mediumaquamarine"}),
+				MyTitleFactory({title: "its ok if you use semicolons", color: "rebeccapurple"}),
+				MyTitleFactory({title: "i am a title as well", color: "darkblue"})
 			)
 		)
 	}
