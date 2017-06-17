03baf35958599fede968c3c513f92383b1d0cab3
Karina Franco <karinafraest@gmail.com>
2017-06-17 01:25:12 -0500

    second component

---
+++ js/ClientApp.js
@@ -0,0 +1,28 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+	/*all functions must include a render method that returns Markup*/
+	render: function() {
+		return(
+			div(null,
+				h1(null, "check out this component")
+			)
+		)
+	}
+})
+
+	var MyFirstComponent = React.createClass({
+		render: function(){
+			return (
+				div(null,
+					React.createElement(MyTitle),
+					React.createElement(MyTitle),
+					React.createElement(MyTitle),
+					React.createElement(MyTitle)
+				)
+			)
+		}
+	})
+
+	ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById("app"))
